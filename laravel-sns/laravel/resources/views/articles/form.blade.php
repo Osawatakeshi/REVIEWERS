c1f642bc7b43ef7d4ce0d0f7a09b46fcfdb98ff0
Osawatakeshi <otakeshi0129@gmail.com>
2020-08-23 20:41:31 +0900

    first commit

---
+++ laravel-sns/laravel/resources/views/articles/form.blade.php
@@ -0,0 +1,22 @@
+@csrf
+<div class="md-form">
+  <label>タイトル</label>
+  <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
+</div>
+<div class="form-group">
+  <article-tags-input
+  :initial-tags='@json($tagNames ?? [])'
+  :autocomplete-items='@json($allTagNames ?? [])'
+  >
+  </article-tags-input>
+</div>
+<div class="form-group">
+  <label></label>
+  <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
+</div>
+<div class="form-group row">
+  <label class="col-md-2">画像</label>
+  <div class="col-md-10">
+    <input type="file" class="form-control-file" name="image">
+  </div>
+</div>
